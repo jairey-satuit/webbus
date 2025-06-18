47ac5e8f88ce00c7bc1c8f989a0e377e2c91beed
jairey <jon.airey@tier1fin.com>
2025-06-18 09:31:27 -0400

    adding bg-color to application state

--- WebBus/Global.asax.cs
+++ WebBus/Global.asax.cs
@@ -16,6 +16,7 @@ namespace WebBus
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["BackgroundColor"] = "#f5aee7";
         }
     }
 }
